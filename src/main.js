aceb58a5e6fb9bd5ee8dc50a338a3b4050076cd6
xzidx <c.samnang.de@gmail.com>
2026-01-28 14:47:49 +0700

    feat/header/footer

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+/* --- Font Awesome Setup --- */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Import all icons in ONE statement to keep it clean
+import { 
+  faCoffee, 
+  faUser, 
+  faBars, 
+  faMagnifyingGlassLocation ,
+  faRightToBracket
+} from '@fortawesome/free-solid-svg-icons'
+
+// Add them all to the library at once
+library.add(faCoffee, faUser, faBars, faMagnifyingGlassLocation,faRightToBracket)
+
+/* --- App Initialization --- */
+const app = createApp(App)
+
+app.use(router)
+
+// Register the component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+
+<template>
+  <main>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </main>
+</template>
