e1251addbb6cb8a487c32145952a0dcc01c325cc
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-01-29 09:09:56 +0700

    test commit

--- src/views/WarningView.vue
+++ src/views/WarningView.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="">hello world
-
+  <div class="min-h-screen bg-[#CAF0F8] flex justify-center pt-20 p">
+    <div class="bg-amber-100 p-4 mx-auto">
+      <p class="text-black text-[34px] font-black ma">Weather warnings in Norway</p>
     </div>
+  </div>
 </template>
+
