83410aa1309691ae5d52095d5c684f584c92e8b7
xzidx <c.samnang.de@gmail.com>
2026-01-29 10:24:39 +0700

    fixed feat/header/footer

--- src/main.js
+++ src/main.js
@@ -9,16 +9,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Import all icons in ONE statement to keep it clean
-import { 
-  faCoffee, 
-  faUser, 
-  faBars, 
-  faMagnifyingGlassLocation ,
-  faRightToBracket
+import {
+  faCoffee,
+  faUser,
+  faBars,
+  faMagnifyingGlassLocation,
+  faRightToBracket,
 } from '@fortawesome/free-solid-svg-icons'
 
 // Add them all to the library at once
-library.add(faCoffee, faUser, faBars, faMagnifyingGlassLocation,faRightToBracket)
+library.add(faCoffee, faUser, faBars, faMagnifyingGlassLocation, faRightToBracket)
 
 /* --- App Initialization --- */
 const app = createApp(App)
