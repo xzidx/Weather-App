bb3b278c86080f616ebe59e1555a462854ac1e0c
sreanlita81-cpu <sreanlita81@gmail.com>
2026-02-05 15:59:20 +0700

    feat/Helpview

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,51 +1,47 @@
 <template>
-  <!-- Image on top -->
-  <div class="absolute top-0 left-1/2 -translate-x-1/2 z-5 ">
-    <img
-      src="@/assets/150.jpg"
-      alt="Weather Logo"
-      class="h-100 w-400 max-w-6xl transition-transform shadow-lg"
-    />
-  </div>
+  <div class="bg-white w-full min-h-screen gap-16 flex flex-col items-center justify-center py-12">
+    
+    <div class="z-10 mb-8">
+      <img
+        src="@/assets/150.jpg"
+        alt="Weather Logo"
+        class="h-100 w-500 max-w-6xl transition-transform shadow-lg"
+      />
+    </div>
 
-  <!-- Text content -->
-  <div class="relative z-10 flex justify-center pt-[350px]">
-    <div class="w-7xl h-full px-12 py-12 space-y-8"> 
+    <div class="w-full max-w-5xl px-12 space-y-8"> 
       
-      <!-- Top Buttons -->
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-        <button class="w-full bg-blue-200 border border-blue-800 text-blue-600
-                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-50 hover:shadow-md transition">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 ">
+        <button class="w-full bg-blue-100 border border-blue-300 text-blue-800
+                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-200 transition">
           Locations and weather symbols
+          
         </button>
-        <button class="w-full bg-blue-200 border border-blue-800 text-blue-600
-                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-50 hover:shadow-md transition">
+        <button class="w-full bg-blue-100 border border-blue-300 text-blue-800
+                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-200 transition">
           Yr, the API and our privacy policy
         </button>
-        <button class="w-full bg-blue-200 border border-blue-800 text-blue-600
-                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-50 hover:shadow-md transition">
+        <button class="w-full bg-blue-100 border border-blue-300 text-blue-800
+                       rounded-lg px-4 py-3 text-sm font-medium hover:bg-blue-200 transition">
           App for iOS and Android
         </button>
       </div>
 
-      <!-- Links -->
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 text-xs text-black">
-        <div class="space-y-1.5">
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Global 3-day precipitation map</p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Wind map</p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Questions regarding location names</p>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-10 text-xs text-slate-800 ">
+        <div class="space-y-3 ​">
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Global 3-day precipitation map</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Wind map</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Questions regarding location names</p>
         </div>
-        <div class="space-y-1.5">
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">The 21-day forecast on Yr</p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Colour coded weather warnings</p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">FAQ: Frequently asked questions</p>
+        <div class="space-y-3">
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 ​ cursor-pointer text-decoration-line: underline ">The 21-day forecast on Yr</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer ​text-decoration-line: underline ">Colour coded weather warnings</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600  cursor-pointer ​text-decoration-line: underline ">FAQ: Frequently asked questions</p>
         </div>
-        <div class="space-y-1.5">
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Lightning map: Realtime display</p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">
-            Live precipitation forecast (only for the Nordic countries)
-          </p>
-          <p class="border-b py-1.5 hover:text-blue-600 cursor-pointer">Privacy policy</p>
+        <div class="space-y-3">
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Lightning map: Realtime display</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Live precipitation forecast</p>
+          <p class="border-b border-gray-100 py-6 hover:text-blue-600 cursor-pointer​text-decoration-line: underline ">Privacy policy</p>
         </div>
       </div>
 
